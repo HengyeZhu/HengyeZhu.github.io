17bfac44f7c1214403cd671175ac9a8a50ed7f48
Hengye Zhu <gluciferd@sjtu.edu.cn>
2025-08-12 21:49:12 +0800

    update About_ME.md

--- pelicanconf.py
+++ pelicanconf.py
@@ -36,8 +36,8 @@ MINIMALXY_START_YEAR = 2025
 MINIMALXY_CURRENT_YEAR = date.today().year
 
 # Author settings
-AUTHOR_INTRO = 'Hi! I am Hengye Zhu - an undergraduate student at Shanghai Jiao Tong University. I am interested in NeuroAI.'
-AUTHOR_DESCRIPTION = 'Hengye Zhu is an undergraduate student at Shanghai Jiao Tong University. He is interested in NeuroAI, Applied Mathematics and Control Theory. His research experience includes developing computational neuronal models and brain-inspired algorithms. In his free time, Hengye enjoys playing badminton and writing tech blogs.' 
+AUTHOR_INTRO = 'Hi! I am Hengye Zhu - an undergraduate student at Shanghai Jiao Tong University. I am interested in HPC.'
+AUTHOR_DESCRIPTION = 'Hengye Zhu is an undergraduate student at Shanghai Jiao Tong University. He is interested in Computational Neuroscience and HPC.' 
 AUTHOR_AVATAR = 'https://avatars.githubusercontent.com/u/186930454?s=400&u=0bc1b19dad19038c8d03179fd57ac30cf4b71f15&v=4'
 AUTHOR_WEB = 'https://github.com/HengyeZhu'
 
